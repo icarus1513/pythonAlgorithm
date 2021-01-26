d4882f5247b84850419ee2e80f482f911233b289
yonghyun <rladydgus987@naver.com>
2021-01-26 18:14:42 +0900

    위장

---
+++ Level2/위장.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    answer = 1
+    dict = {}
+    for i in clothes:
+        if i[1] in dict:
+            dict[i[1]] = dict[i[1]] + 1
+        else:
+            dict[i[1]] = 1
+
+    for i in dict.values():
+        answer *= i + 1
+
+    return answer - 1
