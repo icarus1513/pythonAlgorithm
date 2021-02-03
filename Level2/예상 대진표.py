23950800920ec464d12dddc538c644c74207d664
yonghyun <rladydgus987@naver.com>
2021-02-03 21:02:57 +0900

    예상 대진표.py

---
+++ Level2/예상 대진표.py
@@ -0,0 +1,8 @@
+def solution(n,a,b):
+    # answer = 0
+    # while a != b :
+    # answer += 1
+    # a, b = (a+1)//2, (b+1)//2
+    # return answer
+    print(((a-1)^(b-1)).bit_length())
+    return ((a-1)^(b-1)).bit_length()
